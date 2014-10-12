8127058562e0cb2d70f2524b02071a3cd778a36d
wfheredia <holatk2003@yahoo.com.ar>
2014-10-11 22:26:13 -0300

    carga de plugin nfc y barcodescanner

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,23 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
+        "id": "com.chariotsolutions.nfc.plugin.NFC",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.chariotsolutions.nfc.plugin": "0.5.1",
+    "com.phonegap.plugins.barcodescanner": "2.0.0"
+}
 // BOTTOM OF METADATA
 });
